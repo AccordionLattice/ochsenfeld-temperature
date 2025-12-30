c7ed86e1f22b41643b3e6c50f25ef9479256a098
AccordionLattice <andreas.meyer@icfo.eu>
2025-12-30 15:03:49 +0100

    Included radiation in prediction

--- app.py
+++ app.py
@@ -14,9 +14,10 @@ app = Flask(__name__)
 # -------------------------------
 # Config
 # -------------------------------
-CACHE_DURATION = timedelta(hours=12)
+CACHE_DURATION = timedelta(hours=6)
 FORECAST_FILE = "cached_forecast.csv"
 VALUES_FILE = "cached_values.csv"
+SUN_FILE = "cached_sun.csv"
 
 def needs_update(filename):
     if not os.path.exists(filename):
@@ -38,42 +39,54 @@ def index():
     forecast["date"] = pd.to_datetime(forecast["date"])
     values = pd.read_csv(VALUES_FILE)
     values["date"] = pd.to_datetime(values["date"])
+    sun = pd.read_csv(SUN_FILE)
+    sun["date"] = pd.to_datetime(sun["begin"]).dt.tz_localize(None)
 
     # -------------------------------
     # Simulation
     # -------------------------------
-    def simulate(params, air, time):
+    def simulate(params, air, radiation, time):
         k_w, k_f, k_s = params
         N = len(air)
         Tin = np.zeros(N)
         Tin[0] = 3
         ground = 15
+        
+        if len(radiation) < N:
+            extrapoled = np.ones(N) * np.mean(radiation[-24*3:])
+            extrapoled[:len(radiation)] = radiation
+            radiation = extrapoled
 
         for n in range(N - 1):
             dt = (time[n+1] - time[n]).total_seconds() / 3600
             Tin[n+1] = Tin[n] + dt * (
                 k_w * (air[n] - Tin[n]) +
-                k_f * (ground - Tin[n])
+                k_f * (ground - Tin[n]) +
+                k_s * radiation[n]
             )
         return Tin
 
-    k_w, k_f, k_s = 0.00875756, 0.0007412, 0.00011976
+    k_w, k_f, k_s = 0.00875756, 0.0007412, 0.00011976 
     merged = pd.concat([values, forecast], ignore_index=True)
-    Tin = simulate([k_w, k_f, k_s], merged["value"], merged["date"])
+    mask = (sun['date'] >= values['date'].iloc[0]) & (sun['date'] <= values['date'].iloc[-1])
+    radiation = sun.loc[mask, 'v'].to_numpy()
+    radiation = np.nan_to_num(radiation)
+    Tin = simulate([k_w, k_f, k_s], merged["value"], radiation, merged["date"])
 
     # -------------------------------
     # Plot
     # -------------------------------
-    plt.figure(figsize=(10, 4))
+    plt.figure(figsize=(8, 4))
     plt.plot(values['date'], values['value'], label="Aussentemperatur", color="dimgrey")
     plt.plot(forecast['date'], forecast['value'], color="lightgrey")
     plt.plot(merged["date"], Tin, label="Bad (erwartet)", color="tab:blue")
     plt.axhline(0, color="gray", linestyle="-")
-    plt.ylabel("Temperature (°C)")
+    plt.ylabel("Temperatur (°C)")
     plt.legend()
     plt.xlim(left=merged['date'].min(), right=merged['date'].max())
     plt.xticks(rotation=30)
     plt.tight_layout()
+    plt.grid() 
 
     # Save plot
     if not os.path.exists("static"):
@@ -82,3 +95,6 @@ def index():
     plt.close()
 
     return render_template("index.html")
+
+# if __name__ == "__main__":
+#     app.run(debug=True, port=5000, use_reloader=False)
--- update_data.py
+++ update_data.py
@@ -5,6 +5,8 @@ from wetterdienst import Settings
 from wetterdienst.provider.dwd.observation import DwdObservationRequest
 from datetime import datetime, timedelta
 
+def floor_to_hour(dt):
+    return dt.replace(minute=0, second=0, microsecond=0)
 
 def update_data():
     # -------------------------------
@@ -12,6 +14,15 @@ def update_data():
     # -------------------------------
     METEOBLUE_URL = "https://my.meteoblue.com/packages/basic-1h?apikey=yV0eEcwKxnDWtavX&lat=48.8829&lon=11.2338&asl=408&format=csv&windspeed=ms-1"
     response = requests.get(METEOBLUE_URL)
+    
+    response.raise_for_status()  # raises error if request failed
+
+    if "temperature" not in response.text:
+        raise ValueError("Meteoblue CSV not returned. Check API key and URL.")
+
+    forecast = pd.read_csv(StringIO(response.text))
+
+
     forecast = pd.read_csv(StringIO(response.text))
     forecast = forecast.rename(columns={"temperature": "value", "time": "date"})
     forecast["date"] = pd.to_datetime(forecast["date"])
@@ -35,5 +46,29 @@ def update_data():
     )
     station = request.filter_by_station_id(station_id=(1161,))
     values = station.values.all().df.to_pandas()
+    if values.empty:
+        raise ValueError("No DWD data returned for station 1161.")
+
     values["date"] = pd.to_datetime(values["date"]).dt.tz_localize(None)
     values.to_csv("cached_values.csv", index=False)
+    
+    # -------------------------------
+    # Sensoto sun data
+    # -------------------------------
+    end = datetime.utcnow()
+    start = end - timedelta(days=31)
+
+    end = floor_to_hour(end)
+    start = floor_to_hour(start)
+    
+    interval = f"R{365*24}/{start.strftime('%Y-%m-%dT%H:%M:%SZ')}/PT1H"
+    url = "https://api.sensoto.io/v1/organizations/open/networks/gkd-bayern/devices/200010/sensors/meteo-globalstrahlung/measurements"
+
+    params = {
+        "interval": interval
+    }
+
+    r = requests.get(url, params=params)
+    r.raise_for_status()
+    sun = pd.DataFrame(r.json())
+    sun.to_csv("cached_sun.csv", index=False)
